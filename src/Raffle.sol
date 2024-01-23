064c51eb516ac0f240550b5d3466dd8f568c27b4
nathanbrews <nattnukm@gmail.com>
2024-01-23 18:01:06 +0000

    setup

---
+++ src/Raffle.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+/**
+ * @title Sample Raffle Contract
+ * @author Nathaniel Brewster
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2
+ */
+contract Raffle {
+    uint256 private s_enteranceFee;
+    
+    constructor(uint256 entranceFee) {
+        i_enteranceFee = entranceFee;
+    }
+    function enterRaffle()  public payable () {
+        
+    }
+    function pickWinner() public () {}
+    /** Getter Function */
+    function getEnteranceFee() external view returns(uint256){
+        return i_enteranceFee;
+    }
+}
